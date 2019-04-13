b562f509a7758a192ac1f097c4e35657a99f0d5d
sbx_user1051 <sbx_user1051@169.254.8.149>
2019-04-13 14:04:39 +0000

    Sat Apr 13 14:04:39 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,15 @@
-
+from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    return 10
+    skuCounter = Counter(skus)
+    for item in skuCounter:
+        if item not in ["A", "B", "C", "D"]:
+            return -1
+    for skuKey in skuCounter:
+        if skuKey == "D":
+
+
+
 
