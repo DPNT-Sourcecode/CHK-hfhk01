e08b5cb43d831f1aabf984d2c6b2cab879d56887
sbx_user1051 <sbx_user1051@169.254.8.149>
2019-04-13 14:07:39 +0000

    Sat Apr 13 14:07:39 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,11 +4,23 @@ from collections import Counter
 # skus = unicode string
 def checkout(skus):
     skuCounter = Counter(skus)
+    price = 0
     for item in skuCounter:
         if item not in ["A", "B", "C", "D"]:
             return -1
     for skuKey in skuCounter:
         if skuKey == "D":
+            price += skuCounter[skuKey]*15
+        elif skuKey == "C":
+            price += skuCounter[skuKey]*20
+        elif skuKey == "B":
+            if skuCounter[skuKey] % 2 == 0:
+                price += (skuCounter[skuKey]/2) * 45
+            else:
+                price += (skuCounter[skuKey]-1/2) * 130
+                price += 50
+            
+
 
 
 
