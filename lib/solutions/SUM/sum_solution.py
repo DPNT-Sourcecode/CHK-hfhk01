dedbf3ce5f99b7dbd1ad874a734621e7f1ca3a08
sbx_user1051 <sbx_user1051@169.254.8.149>
2019-04-13 13:45:58 +0000

    Sat Apr 13 13:45:58 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,14 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    """Returns a + b where both are integers
+
+    >>> compute(5, 10)
+    10
+
+    """
+    return x + y
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
